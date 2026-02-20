aeac9831f6daed896e85fdcb5ac865b62fb36248
MohammadReza <sirmohammadra@gmail.com>
2026-02-20 21:57:11 +0330

    fix(user): replace , with ; for separate tags

--- services/authentication/internal/user/model.go
+++ services/authentication/internal/user/model.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name        string `gorm:"type:varchar(30),not null" json:"username"`
-	PhoneNumber string `gorm:"type:varchar(13),not null,unique" json:"phone_number"`
-	Password    string `gorm:"type:varchar(255),not null" json:"_"`
+	Name        string `gorm:"type:varchar(30);not null" json:"username"`
+	PhoneNumber string `gorm:"type:varchar(13);not null,unique" json:"phone_number"`
+	Password    string `gorm:"type:varchar(255);not null" json:"_"`
 }
