2858e7a88e7c38bced8d56657c28935d1f51cd5f
MohammadReza <sirmohammadra@gmail.com>
2026-02-21 17:33:43 +0330

    feat(proto): add user and token proto files and compile them

---
+++ services/authentication/internal/grpc/token_handler.go
@@ -0,0 +1,26 @@
+package grpc
+
+import (
+	"authentication/internal/token"
+	proto "authentication/internal/token/proto/token"
+	"context"
+)
+
+type TokenHandler struct {
+	proto.UnimplementedTokenServiceServer
+	tokenService *token.TokenService
+}
+
+func NewTokenHandler(ts *token.TokenService) *TokenHandler {
+	return &TokenHandler{
+		tokenService: ts,
+	}
+}
+
+func (th *TokenHandler) IsTokenValid(c context.Context, req *proto.CheckTokenRequest) (*proto.CheckTokenResponse, error) {
+	response := th.tokenService.IsTokenValid(req.Token)
+
+	return &proto.CheckTokenResponse{
+		Valid: response,
+	}, nil
+}
---
+++ services/authentication/internal/grpc/user_handler.go
@@ -0,0 +1,52 @@
+package grpc
+
+import (
+	"authentication/internal/user"
+	proto "authentication/internal/user/proto/user"
+	"context"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type UserHandler struct {
+	proto.UnimplementedUserServiceServer
+	userService *user.UserService
+}
+
+func NewUserHandler(us *user.UserService) *UserHandler {
+	return &UserHandler{
+		userService: us,
+	}
+}
+
+func (uh *UserHandler) Login(c context.Context, req *proto.LoginRequest) (*proto.AuthResponse, error) {
+	response, err := uh.userService.Login(c, req.PhoneNumber, req.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.AuthResponse{
+		RefreshToken: response.RefreshToken,
+		AccessToken:  response.AccessToken,
+	}, nil
+}
+
+func (uh *UserHandler) Logout(c context.Context, req *proto.LogoutRequset) (*emptypb.Empty, error) {
+	if err := uh.userService.Logout(c, req.Token); err != nil {
+		return nil, err
+	}
+
+	return &emptypb.Empty{}, nil
+}
+
+func (uh *UserHandler) Register(c context.Context, req *proto.RegisterRequest) (*proto.AuthResponse, error) {
+	response, err := uh.userService.RegisterUser(c, req.Name, req.PhoneNumber, req.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.AuthResponse{
+		RefreshToken: response.RefreshToken,
+		AccessToken:  response.AccessToken,
+	}, nil
+}
